b16adfa15a6ed69961b3659d4ddcdde666ad724b
SuzanWebDeveloper <iamstrongwebdev@gmail.com>
2024-03-24 20:04:18 +0300

    basic structure

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace InventoryManagement
+{
+
+  public class Item
+  {
+    public string Name
+    {
+      get;
+    }
+
+    public int Quantity
+    {
+      get;
+      set;
+    }
+    public DateTime createdDate { get; set; }
+
+    public Item(string name, int quantity, DateTime createdDate = default)
+    {
+      if (quantity < 0)
+      {
+        //error argument exception
+      }
+      Name = name;
+      Quantity = quantity;
+      createdDate = createdDate == default ? DateTime.Now : createdDate;
+    }
+
+  }
+
+
+  public class Store
+  {
+    private List<Item> items = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+      //Do not allow to add items with same name to the store
+
+    }
+    public void DeleteItem(string itemName)
+    {
+
+    }
+    public static int GetCurrentVolume()
+    {
+      //compute the total amount of items in the store
+      return 0;
+    }
+    public static void FindItemByName(string name)
+    {
+      // find an item by name.
+    }
+    public static void SortByNameAscto(string name)
+    {
+      // get the sorted collection by name in ascending order.
+    }
+  }
+}
+
+
